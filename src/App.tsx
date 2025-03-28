a5dbcbf4de91f0d5ed4598e187d6ab9428a649a0
Anton B <antonbsa.bck@gmail.com>
2025-03-27 22:38:20 -0300

    chore(setup): add styled-components for theming and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/theme/default'
+import { GlobalStyle } from './styles/global'
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
